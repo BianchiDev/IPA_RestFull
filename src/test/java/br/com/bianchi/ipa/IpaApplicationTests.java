27d14bf3a17177bb64588c7619137caae41d285f
BianchiDev <ifpebianchi@gmail.com>
2023-08-28 06:55:24 -0300

    first commit

---
+++ src/test/java/br/com/bianchi/ipa/IpaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bianchi.ipa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
